51f5bb2d30a3de0dea2b344b70f11e236c992ca9
esmvcs <esmvcs@rit.edu>
2025-01-29 13:38:00 -0500

    W 1/29 XOR, shifting, comp., & XOR swap - 1pm demo

--- 1pm-Demos/BitwiseOperators/BitwiseOperators.cpp
+++ 1pm-Demos/BitwiseOperators/BitwiseOperators.cpp
@@ -29,9 +29,9 @@ int main()
 	cout << "\n\n\n";
 
 	// *************************************************************************************
-	// Bitwise AND and OR
+	// Bitwise AND, OR & XOR
+	// comparisons
 	// *************************************************************************************
-
 
 	// Bitwise AND - &
 	// If BOTH bits are 1, the resulting bit is 1. Otherwise 0.
@@ -54,5 +54,97 @@ int main()
 	printTwoShorts(a, b);
 	printOneShort("a | b", a | b);
 	cout << "\n\n\n";
+
+	// Bitwise XOR - ^
+	// If ONE bit is 1, resulting bit is 1
+	// Effectively -- are they different, bit by bit
+	// 1 1 1 1 0 0 0 0
+	// 1 0 1 0 1 0 1 0
+	// ~~~~~~~~~~~~~~~~
+	// 0 1 0 1 1 0 1 0
+	cout << "Bitwise XOR - ^" << endl;
+	printTwoShorts(a, b);
+	printOneShort("a ^ b", a ^ b);
+	cout << "\n\n\n";
+
+	// *************************************************************************************
+	// Bitwise shifting & complement
+	// *************************************************************************************
+
+	// Shift left
+	// - Move every bit left by #
+	// Decimal shift: 20 <10< 1 ---> 200
+	// Bit shift: 0000 0010 << 1 ---> 0000 0100 
+	// Each shift effectively multiples by 2
+	cout << "Shift left - << " << endl;
+	//a = 256;
+	printOneShort("a", a);
+	printOneShort("a << 1", a << 1);
+	printOneShort("a << 2", a << 2);
+	cout << "\n\n\n";
+
+	// Shift right
+	// - Move every bit right by #
+	cout << "Shift right - >> " << endl;
+	printOneShort("a", a);
+	printOneShort("a >> 1", a >> 1);
+	printOneShort("a >> 2", a >> 2);
+	cout << "\n\n\n";
+
+	short c = 270;
+	printOneShort("c", c);
+	printOneShort("c >> 1", c >> 1);
+	printOneShort("c >> 2", c >> 2);
+	printOneShort("c >> 3", c >> 3);
+	printOneShort("c >> 4", c >> 4);
+	printOneShort("c >> 5", c >> 5);
+	cout << "\n\n\n";
+
+	// Complement
+	// - Inverts each bit
+	// 1 1 1 1 0 0 0 0
+	// 0 0 0 0 1 1 1 1
+	cout << "Complement - ~" << endl;
+	printOneShort("a", a);
+	printOneShort("~a", ~a);
+	cout << "\n\n\n";
+	cout << "\n\n\n";
+
+	// *************************************************************************************
+	// Usage example 1
+	// *************************************************************************************
+
+	short val1 = 0b1111000011110000;
+	short val2 = 0b0000101000001010;
+
+	// typically:
+	/*
+	short tmp = val1;
+	val1 = val2;
+	val2 = tmp;
+	*/
+
+	// NOT allowed to use a 3rd variable.
+
+	// XOR Swap
+	printOneShort("val1", val1);
+	printOneShort("val2", val2);
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+
+	// Create a conversion mask
+	val1 = val1 ^ val2; // tells us how to get from 1 value to the other
+	printOneShort("val1 = val1 ^ val2", val1);
+
+	// Convert val2 --> val1
+	val2 = val2 ^ val1; // apply the mask to val2;
+	printOneShort("val2 = val2 ^ val1", val2);
+
+	// Convert *original* val1 (now in val2) --> val2
+	val1 = val2 ^ val1; // apply the mask to val2 (orig val1);
+	printOneShort("val1 = val2 ^ val1", val1);
+
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	printOneShort("val1", val1);
+	printOneShort("val2", val2);
 
 }
